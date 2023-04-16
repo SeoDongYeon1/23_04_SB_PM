308621453867daa1cb19630093f3bec6db1538e9
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-16 22:30:48 +0900

    myBatis, JDBC 드라이버 추가, yml 파일에 접속 정보 추가, myBatis 활용해서 select, insert, delete, update 구현

--- src/main/java/com/KoreaIT/sdy/demo/repository/ArticleRepository.java
+++ src/main/java/com/KoreaIT/sdy/demo/repository/ArticleRepository.java
@@ -1,62 +1,31 @@
 package com.KoreaIT.sdy.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.KoreaIT.sdy.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	private int lastArticle;
-	private List<Article> articles;
+@Mapper
+public interface ArticleRepository {
 	
-	ArticleRepository() {
-		lastArticle = 0;
-		articles = new ArrayList<>();
-	}
+	@Select("SELECT * FROM article ORDER BY id DESC")
+	public List<Article> getArticles();
 	
-	public void makeTestData() {
-		for(int i = 1; i<=10; i++) {
-			String title = "제목"+i;
-			String body = "내용"+i;
-			
-			writeArticle(title, body);
-		}
-	}
+	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticleById(int id);
+	
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(int id);
 
-	public Article getArticleById(int id) {
-		for(Article article : articles) {
-			if(article.getId()==id) {
-				return article;
-			}
-		}
-		return null;
-	}
-	
-	public void deleteArticle(int id) {
-		Article article = getArticleById(id);
-		articles.remove(article);
-	}
-	
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticleById(id);
-		article.setTitle(title);
-		article.setBody(body);
-	}
+	@Update("UPDATE article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
+	public void modifyArticle(int id, String title, String body);
 
-	public Article writeArticle(String title, String body) {
-		int id = lastArticle+1;
-		
-		Article article = new Article(id, title , body);
-		articles.add(article);
-		lastArticle++;
-		
-		return article;
-	}
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public Article writeArticle(String title, String body);
 
-	public List<Article> getArticles() {
-		return articles;
-	}
 }
