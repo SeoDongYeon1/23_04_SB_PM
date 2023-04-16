679ffff787c5518e6117f378396184e1f2be3737
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-16 21:23:26 +0900

    게시글 수정, 삭제, 상세보기 구현

--- src/main/java/com/KoreaIT/sdy/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/sdy/demo/controller/UsrArticleController.java
@@ -19,7 +19,7 @@ public class UsrArticleController {
 		articles = new ArrayList<>();
 	}
 	
-	@RequestMapping("/usr/article/dowrite")
+	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public Article doWrite(String title, String body) {
 		int id = lastArticle+1;
@@ -35,5 +35,60 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles(String title, String body) {
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+		Article article = getArticleById(id);
+
+		if(article==null) {
+			return id + "번 글은 존재하지 않습니다.";
+		}
+		return article;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticleById(id);
+		
+		if(article==null) {
+			return id + "번 게시글은 존재하지 않습니다.";
+		}
+		deleteArticle(id);
+		return id + "번 게시글을 삭제했습니다.";
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+		Article article = getArticleById(id);
+		
+		if(article==null) {
+			return id + "번 게시글은 존재하지 않습니다.";
+		}
+		modifyArticle(id, title, body);
+		return id + "번 게시글을 수정했습니다." + article;
+	}
+	
+	public Article getArticleById(int id) {
+		for(Article article : articles) {
+			if(article.getId()==id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticleById(id);
+		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticleById(id);
+		article.setTitle(title);
+		article.setBody(body);
 	}
 }
